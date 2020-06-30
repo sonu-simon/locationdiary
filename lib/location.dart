80bd4f688fa33d386ee4488a4d00509feb2f9372
sonu-simon <sonuisplaying@gmail.com>
2020-06-30 18:47:23 +0530

    kind of object done

---
+++ lib/location.dart
@@ -0,0 +1,51 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:intl/intl.dart';
+
+class LocationData {
+  Position currentPosition;
+  String locURL;
+  String streetName;
+  bool status = false;
+  DateTime now = DateTime.now();
+  var formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now());
+
+  getCurrentLocation() async {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+    double lat;
+    double lng;
+
+    //Get current location
+    await geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      currentPosition = position;
+      lat = currentPosition.latitude;
+      lng = currentPosition.longitude;
+      print(currentPosition);
+    }).catchError((e) {
+      print(e);
+    });
+
+    //Get the street name of the current location
+    await geolocator
+        .placemarkFromCoordinates(lat, lng)
+        .then((List<Placemark> placemark) {
+      streetName = placemark[0].name;
+      print(streetName);
+    }).catchError((e) {
+      print(e);
+    });
+
+    status = true;
+
+    //Set URL for redirecting to Google Maps
+    locURL = "https://www.google.com/maps/search/?api=1&query=$lat,$lng";
+    print(locURL);
+  }
+
+  //Launch the URL with map coordindates
+  locURLFn() {
+    launch(locURL);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:geolocator/geolocator.dart';
-import 'package:url_launcher/url_launcher.dart';
+import './location.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,46 +21,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Position _currentPosition;
-  String locURL;
-  String _currentAddress;
-
-  _getCurrentLocation() {
-    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
-    geolocator
-        .placemarkFromCoordinates(
-            _currentPosition.latitude, _currentPosition.longitude)
-        .then((List<Placemark> placemark) {
-      setState(() {
-        _currentAddress = placemark[0].name;
-        print(_currentAddress);
-      });
-    });
-    geolocator
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-      });
-    }).catchError((e) {
-      print(e);
-    });
-  }
-
-  _setLocationURL(String lat, String lng) async {
-    locURL = "https://www.google.com/maps/search/?api=1&query=$lat,$lng";
-  }
-
-  _locURL() {
-    launch(locURL);
-  }
+  LocationData locationData = LocationData();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("GeoLocate"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {
+            setState(() {
+              
+            });
+          })
+        ],
       ),
       body: Center(
         child: Column(
@@ -71,14 +43,17 @@ class _MyHomePageState extends State<MyHomePage> {
             RaisedButton(
               child: Text("Get location"),
               onPressed: () {
-                _getCurrentLocation();
-                _setLocationURL(_currentPosition.latitude.toString(),
-                    _currentPosition.longitude.toString());
+                setState(() {
+                  locationData = LocationData();
+                  locationData.getCurrentLocation();
+                  print(locationData.streetName);
+                });
               },
             ),
-            if (_currentAddress != null) Text(_currentAddress),
-            if (_currentPosition != null)
-              RaisedButton(onPressed: _locURL, child: Text("View in maps"))
+            if (locationData.status == true)
+              Text(locationData.streetName ?? "Waiting for data"),
+            Text(locationData.formattedDate),
+            FlatButton(onPressed: locationData.locURLFn, child: Text("maps"))
           ],
         ),
       ),
